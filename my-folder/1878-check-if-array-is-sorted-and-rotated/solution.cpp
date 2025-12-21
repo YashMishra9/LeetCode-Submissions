8118d1affab753c4531a0cd9e6a35a1bd3d80768
YashMishra9 <anyashmishra@gmail.com>
2025-12-21 13:48:25 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 11.2 MB (83.71%)

--- my-folder/1878-check-if-array-is-sorted-and-rotated/solution.cpp
+++ my-folder/1878-check-if-array-is-sorted-and-rotated/solution.cpp
@@ -4,13 +4,14 @@ public:
         int count=0;
         int n=nums.size();
         for(int i=1;i<n;i++){
-            if(nums[i-1]>nums[i]){
-                count++;
-            }
+                if(nums[i-1]>nums[i]){
+                    count+=1;
+                }
         }
         if(nums[n-1]>nums[0]){
             count++;
         }
         return count<=1;
+                
     }
 };
