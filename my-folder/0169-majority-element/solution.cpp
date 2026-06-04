a83d5872bd263efe308fba62a4c7a0af1cea5ccc
YashMishra9 <anyashmishra@gmail.com>
2026-06-04 18:51:32 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 28.3 MB (39.03%)

--- my-folder/0169-majority-element/solution.cpp
+++ my-folder/0169-majority-element/solution.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     int majorityElement(vector<int>& nums) {
             map<int,int>mpp;
-            for(int i=0;i<nums.size();i++){
-                mpp[nums[i]]++;
+            for(int x:nums){
+                mpp[x]++;
             }
             for(auto it:mpp){
-                if(it.second>(nums.size()/2)){
+                if(it.second>nums.size()/2){
                     return it.first;
                 }
             }
-    return -1;
+            return 0;
     }
             
 };
